35e25bca183893d2a1466932c756b4245c93af7b
Donni Maulana Sipa <35377276+donnim@users.noreply.github.com>
2019-07-09 10:49:20 +0700

    Create top.php

---
+++ top.php
@@ -0,0 +1,5 @@
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<script language="JavaScript" type="text/javascript" src="https://donnim.github.io/web/javascript.js"></script>
