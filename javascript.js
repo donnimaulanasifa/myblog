f75c1650574de8e4235e032e5f791d3f90271d11
Donni Maulana Sipa <35377276+donnim@users.noreply.github.com>
2019-07-09 10:04:42 +0700

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,19 @@
+$(document).ready( function() {
+    $('img').each( function() {
+        if($(this).attr("alt") == "www.000webhost.com") {
+            $(this).remove();
+            console.log("");
+        }
+        else {
+            console.log("")
+        }
+    });
+});
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
