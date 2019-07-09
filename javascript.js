1dd7eebc875dc31164bca4d8aad1a8a438cd519b
Donni Maulana Sipa <35377276+donnim@users.noreply.github.com>
2019-07-09 10:10:13 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
+	jQuery(function($){
+    $(document).ready( function() {
+    $('img').each( function() {
+        if($(this).attr("alt") == "www.000webhost.com") {
+            $(this).remove();
+            console.log("");
+        }
+        else {
+            console.log("")
+        }
+      });
+    });
+});
+    
+
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
